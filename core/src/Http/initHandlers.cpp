23790ba6abf31b1a12534a7256f1a925361156ef
BuNaTin <k9687928424@gmail.com>
2022-10-19 14:20:45 +0300

    Add post handler

--- core/src/Http/initHandlers.cpp
+++ core/src/Http/initHandlers.cpp
@@ -40,6 +40,67 @@ std::string htmlFileList(tinydir_dir &dir, const i32 from = 1) {
     return payload;
 }
 
+http_get::Response getFile(const std::string &shared_folder,
+                           const std::string &req_pattern,
+                           const http_get::Request *req) {
+    std::string filename =
+            shared_folder + "/" +
+            urlDecode(utils::value(req_pattern, req->query()));
+
+    std::fstream fileOrDir(filename);
+    fileOrDir.seekg(0, std::ios::end);
+    tinydir_dir dir;
+    if (fileOrDir.good()) {
+        fileOrDir.close();
+        // return file
+        return http_get::Response().code(200, "Ok").fileData(filename);
+    } else if (tinydir_open_sorted(&dir, filename.c_str()) != -1) {
+        std::string payload = htmlFileList(dir);
+        tinydir_close(&dir);
+
+        // return directory
+        return http_get::Response()
+                .code(200, "Ok")
+                .content("text/html; charset=UTF-8")
+                .data(payload);
+    }
+
+    return http_get::Response()
+            .code(404, "Not found")
+            .content("text/html; charset=UTF-8")
+            .data("<h1>No such file</h1>");
+}
+
+http_get::Response getDir(const std::string &shared_folder,
+                          const http_get::Request *req) {
+    tinydir_dir dir;
+    tinydir_open_sorted(&dir, shared_folder.c_str());
+
+    std::string payload = htmlFileList(dir, 2);
+
+    tinydir_close(&dir);
+
+    return http_get::Response()
+            .code(200, "Ok")
+            .content("text/html; charset=UTF-8")
+            .data(payload);
+}
+
+http_get::Response postFile(const std::string &shared_folder,
+                            const std::string &req_pattern,
+                            const http_get::Request *req) {
+    std::string filename =
+            shared_folder + "/" +
+            urlDecode(utils::value(req_pattern, req->query()));
+
+    req->copyTo(filename);
+
+    return http_get::Response()
+            .code(201, "Created")
+            .content("text/html; charset=UTF-8")
+            .data("<h1>" + filename + "</h1>");
+}
+
 } // namespace
 
 namespace http_get { inline namespace Http {
@@ -48,49 +109,21 @@ void initHanlders(Http::Server *server,
                   const std::string &shared_folder) {
     if (!server) return;
 
+    server->addHandler("GET /%",
+                       [shared_folder](const Http::Request *req) {
+                           return getFile(shared_folder, "GET /%", req);
+                       });
+
+    server->addHandler("GET /",
+                       [shared_folder](const Http::Request *req) {
+                           return getDir(shared_folder, req);
+                       });
+
+    std::string post_handler = "POST /%";
     server->addHandler(
-            "GET /%", [shared_folder](const Http::Request *req) {
-                std::string filename =
-                        shared_folder + "/" +
-                        urlDecode(utils::value("GET /%", req->query()));
-
-                std::fstream fileOrDir(filename);
-                fileOrDir.seekg(0, std::ios::end);
-                tinydir_dir dir;
-                if (fileOrDir.good()) {
-                    fileOrDir.close();
-                    return Http::Response().code(200, "Ok").fileData(
-                            filename);
-                } else if (tinydir_open_sorted(
-                                   &dir, filename.c_str()) != -1) {
-                    std::string payload = htmlFileList(dir);
-                    tinydir_close(&dir);
-
-                    return Http::Response()
-                            .code(200, "Ok")
-                            .content("text/html; charset=UTF-8")
-                            .data(payload);
-                }
-
-                return Http::Response()
-                        .code(404, "Not found")
-                        .content("text/html; charset=UTF-8")
-                        .data("<h1>No such file</h1>");
-            });
-
-    server->addHandler(
-            "GET /", [shared_folder](const Http::Request *req) {
-                tinydir_dir dir;
-                tinydir_open_sorted(&dir, shared_folder.c_str());
-
-                std::string payload = htmlFileList(dir, 2);
-
-                tinydir_close(&dir);
-
-                return Http::Response()
-                        .code(200, "Ok")
-                        .content("text/html; charset=UTF-8")
-                        .data(payload);
+            post_handler,
+            [shared_folder, post_handler](const Http::Request *req) {
+                return postFile(shared_folder, post_handler, req);
             });
 }
 
